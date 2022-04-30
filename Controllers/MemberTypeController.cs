5c6c10607bdc91cd11117ec66766ba8b9a9a2094
p0ppy-fl0w3r <kunai_madara@protonmail.com>
2022-04-30 23:47:42 +0545

    Added member category API.

---
+++ Controllers/MemberTypeController.cs
@@ -0,0 +1,23 @@
+﻿using DvD_Api.Data;
+using DvD_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DvD_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class MemberTypeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MemberTypeController(ApplicationDbContext database)
+        {
+            _db = database;
+        }
+
+        [HttpGet]
+        public IEnumerable<MembershipCategory> GetAllCategory() {
+            return _db.MembershipCategories;
+        }
+    }
+}
